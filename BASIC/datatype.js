e914f68603b958cd3ab1d51a5eec1f551451c3e7
HingeSejal <sejalhinge4@gmail.com>
2024-12-03 13:22:46 +0000

    datatype

--- BASIC/datatype.js
+++ BASIC/datatype.js
@@ -3,3 +3,27 @@
 //alert(4+8) - throw error cause we r using nodejs, not browser
 
 console.log(3+4); console.log("Sejaa") //not a good practise , code readability should be high
+
+let name = "sejal"
+let age = 18
+let isLoggedIn = false
+
+//PRIMITIVE DATATYPES:
+//num
+//bigInt
+//string => ""
+//boolean => true/false
+//undefined
+//symbol => unique - use in react,figma tools
+//null => standalone value
+
+let st = null
+
+//object
+
+console.log(typeof "seja")
+console.log(typeof null) //obj
+console.log(typeof undefined) //undefined
+
+
+
