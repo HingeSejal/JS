11cdd9f666756fcaf05eece9320e0db687e0bfeb
HingeSejal <sejalhinge4@gmail.com>
2025-07-06 12:07:13 +0530

    js methods

--- Garg Tut/methods.js
+++ Garg Tut/methods.js
@@ -1,6 +1,11 @@
+//Builtin functions
+
 const students = ["SEja","Hinge","Rita","Bp"];
 
 function print(n){
     console.log(n);
 }
-students.forEach(print);
+students.forEach(print); //alternative for loop
+
+students.forEach((val) => console.log(val));
+students.map((val) => console.log(val));
