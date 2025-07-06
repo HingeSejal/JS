22ced667baa2ae4b112e80483c66e77a59c7a304
HingeSejal <sejalhinge4@gmail.com>
2025-07-06 20:17:07 +0530

    DOM finished

---
+++ Garg Tut/DOM.js
@@ -0,0 +1,15 @@
+//Document Object Model
+
+alert("Savdhan");
+prompt("ur name?"); //input
+
+// obj=> window 
+window.console.log("Sejal");
+window.alert("hai par dikhta nhi");
+console.log(document); //gives ur code
+console.log(document.body); //returns code in body
+document.title ="work"; //DOM is op => even if u assigned diff title name in html..
+document.write("Hey"); //append at the end of html
+
+
+
--- Garg Tut/methods.js
+++ Garg Tut/methods.js
@@ -7,5 +7,25 @@ function print(n){
 }
 students.forEach(print); //alternative for loop
 
-students.forEach((val) => console.log(val));
-students.map((val) => console.log(val));
+students.forEach((val) => console.log(val)); //doesnt return new array
+students.map((val) => console.log(val));  //returns new array
+
+
+let ans = students.findIndex((num) => num==="Hinge");
+console.log(ans);
+
+students.includes("Hinge");
+
+
+const numbers = [1,2,3,4,5];
+const newArr = numbers.filter((num) => num %2==0);
+console.log(newArr); //even number
+
+let newArr2 = numbers.slice(1,3);
+console.log(newArr2);
+
+let newArr3 = numbers.splice(1,4); //from 1 index to 4 continuous delete
+console.log(newArr3); //give deleted no
+console.log(numbers); //give remained no
+
+
