fcf600280d87e4bc5c3555ca46248d3afa51ce7c
HingeSejal <sejalhinge4@gmail.com>
2025-07-13 17:07:45 +0530

    js exe

---
+++ Namaste JS/thry.js
@@ -0,0 +1,10 @@
+// Everything in JS happens in a execution context
+// javascript is the synchronous , single threaded Language
+
+// single threaded => only one command executed at a Time
+// synchronous single threaded => one cmd at atime in a specific order(one after another)
+
+
+// Execution context has two parts 
+// 1.memory (memory creation)
+// 2.code (code execution)
